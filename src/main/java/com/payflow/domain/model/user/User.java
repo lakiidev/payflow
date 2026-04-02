661ea1038240827ccca9ebce35dff91eff1d2bb7
lakiidev <business.lakidev@gmail.com>
2026-04-02 17:44:44 +0200

    feat: add User entity and repository

---
+++ src/main/java/com/payflow/domain/model/user/User.java
@@ -0,0 +1,42 @@
+package com.payflow.domain.model.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+
+import java.time.LocalDateTime;
+
+import java.util.UUID;
+
+@Entity
+@Table(name="users")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private UUID id;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String passwordHash;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/payflow/infrastructure/persisntence/jpa/UserRepository.java
@@ -0,0 +1,12 @@
+package com.payflow.infrastructure.persisntence.jpa;
+
+import com.payflow.domain.model.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
