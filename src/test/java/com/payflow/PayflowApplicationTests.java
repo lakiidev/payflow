edd7392bca2997e0abddf5ef2131ecd2ec829c1c
lakiidev <business.lakidev@gmail.com>
2026-04-06 11:50:18 +0200

    chore: update test configuration to exclude Kafka and use PostgreSQL 18-alpine

--- src/test/java/com/payflow/PayflowApplicationTests.java
+++ src/test/java/com/payflow/PayflowApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration")
 class PayflowApplicationTests {
 
 	@Test
--- src/test/java/com/payflow/TestcontainersConfiguration.java
+++ src/test/java/com/payflow/TestcontainersConfiguration.java
@@ -3,7 +3,6 @@ package com.payflow;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
-import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
@@ -12,14 +11,8 @@ class TestcontainersConfiguration {
 
 	@Bean
 	@ServiceConnection
-	KafkaContainer kafkaContainer() {
-		return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
-	}
-
-	@Bean
-	@ServiceConnection
 	PostgreSQLContainer postgresContainer() {
-		return new PostgreSQLContainer(DockerImageName.parse("postgres:latest"));
+		return new PostgreSQLContainer(DockerImageName.parse("postgres:18-alpine"));
 	}
 
 }
