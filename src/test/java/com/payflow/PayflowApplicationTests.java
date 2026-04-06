854a0a22c1a338ba7e6e0a134716e4d32758c274
lakiidev <business.lakidev@gmail.com>
2026-04-06 11:58:25 +0200

    chore: update CI configuration to include Kafka bootstrap servers and adjust test properties

--- src/test/java/com/payflow/PayflowApplicationTests.java
+++ src/test/java/com/payflow/PayflowApplicationTests.java
@@ -2,14 +2,16 @@ package com.payflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest(properties = "spring.autoconfigure.exclude=org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration")
+@SpringBootTest(
+        properties = {
+                "spring.autoconfigure.exclude=org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration"
+        },
+        webEnvironment = SpringBootTest.WebEnvironment.NONE
+)
 class PayflowApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
