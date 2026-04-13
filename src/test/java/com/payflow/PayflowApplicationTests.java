626ffbba2803688959b42b13a90eee475fc76c82
lakiidev <business.lakidev@gmail.com>
2026-04-13 14:38:38 +0200

    fix: update Kafka configuration and fix build errors

--- src/test/java/com/payflow/PayflowApplicationTests.java
+++ src/test/java/com/payflow/PayflowApplicationTests.java
@@ -4,12 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@SpringBootTest(
-        properties = {
-                "spring.autoconfigure.exclude=org.springframework.boot.kafka.autoconfigure.KafkaAutoConfiguration"
-        },
-        webEnvironment = SpringBootTest.WebEnvironment.NONE
-)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Import(TestcontainersConfiguration.class)
 class PayflowApplicationTests {
 
