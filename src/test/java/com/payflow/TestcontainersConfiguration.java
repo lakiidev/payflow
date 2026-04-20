7282662e0d33f67676695ab2e248f64ca7c1364e
lakiidev <business.lakidev@gmail.com>
2026-04-20 18:33:54 +0200

    fix: refactor TestcontainersConfiguration to use DynamicPropertyRegistrar for datasource properties

--- src/test/java/com/payflow/TestcontainersConfiguration.java
+++ src/test/java/com/payflow/TestcontainersConfiguration.java
@@ -1,10 +1,10 @@
 package com.payflow;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.devtools.restart.RestartScope;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistrar;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
@@ -12,20 +12,23 @@ import org.testcontainers.utility.DockerImageName;
 @TestConfiguration(proxyBeanMethods = false)
 public class TestcontainersConfiguration {
 
-    @Value("${spring.datasource.write.username}")
-    private String username;
-
-    @Value("${spring.datasource.write.password}")
-    private String password;
-
     @Bean
     @RestartScope
-    @ServiceConnection
     PostgreSQLContainer primaryContainer() {
         return new PostgreSQLContainer(DockerImageName.parse("postgres:18-alpine"))
-                .withDatabaseName("payflow")
-                .withUsername(username)
-                .withPassword(password);
+                .withDatabaseName("payflow");
+    }
+
+    @Bean
+    DynamicPropertyRegistrar datasourceProperties(PostgreSQLContainer primaryContainer) {
+        return registry -> {
+            registry.add("spring.datasource.write.url", primaryContainer::getJdbcUrl);
+            registry.add("spring.datasource.write.username", primaryContainer::getUsername);
+            registry.add("spring.datasource.write.password", primaryContainer::getPassword);
+            registry.add("spring.datasource.read.url", primaryContainer::getJdbcUrl);
+            registry.add("spring.datasource.read.username", primaryContainer::getUsername);
+            registry.add("spring.datasource.read.password", primaryContainer::getPassword);
+        };
     }
 
     @Bean
