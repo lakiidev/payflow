9de75f65dc677f6e350ca557bf5e54cd2e358855
lakiidev <business.lakidev@gmail.com>
2026-04-24 23:57:58 +0200

    feat: add custom serializer and deserializer for Currency in Jackson configuration

--- src/main/java/com/payflow/config/JacksonConfig.java
+++ src/main/java/com/payflow/config/JacksonConfig.java
@@ -1,18 +1,40 @@
 package com.payflow.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.Currency;
 
 @Configuration
 public class JacksonConfig {
 
     @Bean
     public ObjectMapper objectMapper() {
+        SimpleModule currencyModule = new SimpleModule();
+        currencyModule.addSerializer(Currency.class, new JsonSerializer<>() {
+            @Override
+            public void serialize(Currency value, JsonGenerator gen, SerializerProvider provider)
+                    throws IOException {
+                gen.writeString(value.getCurrencyCode());
+            }
+        });
+        currencyModule.addDeserializer(Currency.class, new JsonDeserializer<>() {
+            @Override
+            public Currency deserialize(JsonParser p, DeserializationContext ctx)
+                    throws IOException {
+                return Currency.getInstance(p.getText());
+            }
+        });
+
         return new ObjectMapper()
                 .registerModule(new JavaTimeModule())
+                .registerModule(currencyModule)
                 .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
 }
